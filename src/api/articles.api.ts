57cfdd7117a5888759bbcf5538a15994a7653928
Utkarsh Rastogi <utkarsh.2023ug1077@iiitranchi.ac.in>
2025-12-31 23:27:09 +0530

    backend apis added

--- src/api/articles.api.ts
+++ src/api/articles.api.ts
@@ -1,22 +1,24 @@
 import axios from "axios";
-import type { Article } from "../types/article.type"
+import type { Article } from "../types/article.type";
 
-console.log("API BASE URL:", import.meta.env.VITE_API_BASE_URL);
-
-const api = axios.create({
-    
+const apiBase = axios.create({
   baseURL: import.meta.env.VITE_API_BASE_URL,
-  timeout: 10000
+  timeout: 10000,
 });
-console.log("API BASE URL:", import.meta.env.VITE_API_BASE_URL);
+
+const apiPhase2 = axios.create({
+  baseURL: import.meta.env.VITE_API_BASE_URL_PHASE2,
+  timeout: 10000,
+});
 
 export const getOriginalArticles = async (): Promise<Article[]> => {
-  const res = await api.get("/articles");
-  console.log("Original Articles: ", res);
-  return res.data.data ?? res.data;
+  const res = await apiBase.get("/articles");
+  return res.data;
 };
 
 export const getUpdatedArticles = async (): Promise<Article[]> => {
-  const res = await api.get("api/updated-articles");
-  return res.data.data ?? res.data;
+  const res = await apiPhase2.get("/api/updated-articles");
+
+  console.log("Updated :",res)
+  return res.data;
 };
